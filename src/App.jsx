4f4d500b0887a5ae6811c7e516c36993550c7a08
gerardocrr <gerardo2551.cr@gmail.com>
2024-05-03 21:51:21 -0600

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
       <div>{import.meta.env.SOME_KEY}</div>

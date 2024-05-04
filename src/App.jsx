e5cb6bc7a32005a0aa09278905e8c886c6c11f96
gerardocrr <gerardo2551.cr@gmail.com>
2024-05-03 21:49:08 -0600

    change name variables

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
 
   return (
     <div>
-      <div>{import.meta.env.VITE_SOME_KEY}</div>
-      <div>{import.meta.env.VITE_DB_PASSWORD}</div>
+      <div>{import.meta.env.SOME_KEY}</div>
+      <div>{import.meta.env.DB_PASSWORD}</div>
     </div>
   );
 }

0be3b80811a36d198c2b17329586fc442f0c4974
gerardocrr <gerardo2551.cr@gmail.com>
2024-05-03 22:28:31 -0600

    holaa

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-      <div>{import.meta.env.SOME_KEY}</div>
-      <div>{import.meta.env.DB_PASSWORD}</div>
+      <div>{import.meta.env.VITE_SOME_KEY}</div>
+      <div>{import.meta.env.VITE_DB_PASSWORD}</div>
     </div>
   );
 }
